360d5b380c116b5cee1d6306b7d16c71c0f027f2
unknown <kasiarongiers2001@gmail.com>
2024-01-17 20:36:33 +0200

    [ADDED] navigation tabs

---
+++ navigation.js
@@ -0,0 +1,18 @@
+
+function ShowSelected(event, NavigationSelectionID) {
+  // Hide the sections
+  var sections = document.getElementsByClassName('page-content');
+  for (var i = 0; i < sections.length; i++) {
+      sections[i].classList.remove('active-section');
+  }
+
+  // Display the selected section
+  var selectedSection = document.getElementById(NavigationSelectionID);
+  selectedSection.classList.add('active-section');
+}
+
+function showDefaultSection() {
+  // Show the home section by default
+  var defaultSection = document.getElementById('home-section');
+  defaultSection.classList.add('active-section');
+}
